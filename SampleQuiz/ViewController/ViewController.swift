94bbe48aba05f3b82552c0c3f579974a6be4a235
takutomikan <takutomikan@icloud.com>
2021-09-08 10:44:50 +0900

    スタートボタンでのマイキーの声 スタート待機画面でのドラケンの音声

--- SampleQuiz/ViewController/ViewController.swift
+++ SampleQuiz/ViewController/ViewController.swift
@@ -6,17 +6,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     
+    var mikeysound:AVAudioPlayer?
+    var kensound:AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let soundURL = Bundle.main.url(forResource: "ken", withExtension: "mp3") {
+            do {
+                kensound = try AVAudioPlayer(contentsOf: soundURL)
+                kensound?.play()
+            } catch {
+                print("error")
+            }
+        }
+        
         startButton.layer.borderWidth = 2
         startButton.layer.borderColor = UIColor.black.cgColor
+        
     }
-    
-    
+    @IBAction func btnAction(sender: UIButton) {
+        if let soundURL = Bundle.main.url(forResource: "mikey", withExtension: "mp3") {
+            do {
+                mikeysound = try AVAudioPlayer(contentsOf: soundURL)
+                mikeysound?.play()
+            } catch {
+                print("error")
+            }
+            kensound?.stop()
+        }
+        
+    }
 }
 
